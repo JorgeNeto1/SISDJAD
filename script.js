f650c80d8a987e223f76f24e7c9012c55d46bf3f
JorgeNeto1 <140768775+JorgeNeto1@users.noreply.github.com>
2024-11-10 19:59:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.getElementById("toggle-dark-mode").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+window.onload = function() {
+    document.getElementById("loader").style.display = "none";
+};
+
+document.getElementById("toggle-dark-mode").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+
+
+
+
